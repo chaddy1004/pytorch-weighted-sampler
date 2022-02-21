fdfc9d15b482019b197fc2e10720471fcd57b985
Chaddie <chaddie.paik@webtoonscorp.com>
2022-02-21 23:17:49 +0900

    add test2

--- main.py
+++ main.py
@@ -1,27 +1,87 @@
-from weighted_sampler import OverSampler, UnderSampler, NormalSampler, VanillaDataset
+from weighted_sampler import OverSampler, UnderSampler, NormalSampler, VanillaDataset, VanillaDataset2
 from collections import defaultdict
 
 
-def test(sampler, name):
-    freqs = defaultdict(int)
-    for item in sampler:
-        freqs[int(item)] += 1
-
-    print(name)
-    print(f"n_negatives(0): {freqs[0]}, n_positives(1): {freqs[1]}")
-    print(f"total: {freqs[0] + freqs[1]}")
-    print("\n")
-
-
-if __name__ == '__main__':
+def test1():
+    # test for checking different types of sampler
     dataset = VanillaDataset(ratio=10)
-
     print(f"n_negatives (0): {dataset.n_negatives}, n_positives (1): {dataset.n_positives}")
 
     over_sampler = OverSampler(dataset=dataset)
     under_sampler = UnderSampler(dataset=dataset)
     normal_sampler = NormalSampler(dataset=dataset)
+    # checks for oversampler and undersampler
+    samplers = [("oversampler", over_sampler), ("undersampler", under_sampler), ("normalsampler", normal_sampler)]
+    for name, sampler in samplers:
+        freqs = defaultdict(int)
+        for item in sampler:
+            _, label = item
+            freqs[int(label)] += 1
 
-    test(over_sampler, name="oversampler")
-    test(under_sampler, name="undersampler")
-    test(normal_sampler, name="normalsampler")
+        print(name)
+        print(f"n_negatives(0): {freqs[0]}, n_positives(1): {freqs[1]}")
+        print(f"total: {freqs[0] + freqs[1]}")
+        print(f"Original Dataset Size: {len(sampler.dataset)}")
+        print("\n")
+
+
+def dict_to_sorted_list(dict):
+    final = []
+    for data, freq in dict.items():
+        final.append((data, freq))
+
+    return sorted(final, key=lambda x:x[0])
+
+
+def test2():
+    # test for checking different types of sampler
+    dataset = VanillaDataset(len_normal=5, ratio=2)
+    print(f"n_negatives (0): {dataset.n_negatives}, n_positives (1): {dataset.n_positives}")
+
+    num_epochs = 3
+
+    over_sampler = OverSampler(dataset=dataset)
+    under_sampler = UnderSampler(dataset=dataset)
+    normal_sampler = NormalSampler(dataset=dataset)
+    # checks for oversampler and undersampler
+    samplers = [("oversampler", over_sampler), ("undersampler", under_sampler), ("normalsampler", normal_sampler)]
+    for name, sampler in samplers:
+        freqs = defaultdict(int)
+        num_datas = []
+        pos_samples = 0
+        neg_samples = 0
+        for epoch in range(num_epochs):
+            num_data = 0
+            epoch_freqs = defaultdict(int)
+            for item in sampler:
+                data, label = item
+                freqs[int(data)] += 1
+                num_data += 1
+                if label == 0:
+                    neg_samples += 1
+                else:
+                    pos_samples += 1
+                epoch_freqs[int(data)] += 1
+            num_datas.append(num_data)
+
+            intermediate = dict_to_sorted_list(epoch_freqs)
+            print(f"{name} epoch {epoch}: {intermediate}")
+
+        print(name)
+        final = dict_to_sorted_list(freqs)
+
+
+
+        print(final)
+        print(f"Original Dataset Size: {len(sampler.dataset)}")
+        print(f"Num datas per epoch: {num_datas}")
+
+        print(f"(AVG per epoch) n neg samples:{neg_samples/5}, n pos samples:{pos_samples/5}")
+        print("*********************************************************************")
+        print("\n")
+
+
+
+if __name__ == '__main__':
+    # test1()
+    test2()
--- weighted_sampler.py
+++ weighted_sampler.py
@@ -22,21 +22,31 @@ class VanillaDataset(Dataset):
     def __init__(self, ratio, len_normal=100) -> None:
         super().__init__()
 
+        self.positive_data = []
+        self.negative_data = []
+
         self.positive_labels = [1 for _ in range(len_normal * ratio)]
         self.negative_labels = [0 for _ in range(len_normal)]
+
+        for i in range(len(self.positive_labels)):
+            self.positive_data.append(i + 1)
+
+        for i in range(len(self.negative_labels)):
+            self.negative_data.append(-1 * (i + 1))
 
         self.n_positives = len(self.positive_labels)
         self.n_negatives = len(self.negative_labels)
 
-        self.shuffled_labels = self.positive_labels + self.negative_labels
+        self.datas = self.positive_data + self.negative_data
+        self.labels = self.positive_labels + self.negative_labels
 
-        random.shuffle(self.shuffled_labels)
+        # random.shuffle(self.shuffled_labels)
 
     def __getitem__(self, index):
-        return self.shuffled_labels[index]
+        return (self.datas[index], self.labels[index])
 
     def __len__(self):
-        return len(self.shuffled_labels)
+        return len(self.labels)
 
 
 class OverSampler(DataLoader):
@@ -57,7 +67,7 @@ class OverSampler(DataLoader):
                          sampler=self.sampler)
 
     def init_sampler(self):
-        labels = self.dataset.shuffled_labels
+        labels = self.dataset.labels
 
         class_counts = {0: self.dataset.n_negatives, 1: self.dataset.n_positives}
         class_weights = defaultdict()
@@ -88,7 +98,7 @@ class UnderSampler(DataLoader):
                          sampler=self.sampler)
 
     def init_sampler(self):
-        labels = self.dataset.shuffled_labels
+        labels = self.dataset.labels
 
         class_counts = {0: self.dataset.n_negatives, 1: self.dataset.n_positives}
         class_weights = defaultdict()
@@ -117,7 +127,7 @@ class NormalSampler(DataLoader):
                          sampler=self.sampler)
 
     def init_sampler(self):
-        labels = self.dataset.shuffled_labels
+        labels = self.dataset.labels
 
         class_counts = {0: self.dataset.n_negatives, 1: self.dataset.n_positives}
         class_weights = defaultdict()
@@ -128,3 +138,27 @@ class NormalSampler(DataLoader):
 
         weights = [class_weights[labels[i]] for i in range(self.n_samples)]
         self.sampler = WeightedRandomSampler(torch.DoubleTensor(weights), self.final_size, replacement=self.replacement)
+
+
+class VanillaDataset2(Dataset):
+    def __init__(self, ratio, len_normal=100) -> None:
+        super().__init__()
+
+        self.positive_labels = [1 for _ in range(len_normal * ratio)]
+        self.negative_labels = [0 for _ in range(len_normal)]
+
+        self.positive_data = []
+        self.negative_data = []
+
+        self.n_positives = len(self.positive_labels)
+        self.n_negatives = len(self.negative_labels)
+
+        self.shuffled_labels = self.positive_labels + self.negative_labels
+
+        random.shuffle(self.shuffled_labels)
+
+    def __getitem__(self, index):
+        return self.shuffled_labels[index]
+
+    def __len__(self):
+        return len(self.shuffled_labels)
